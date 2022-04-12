8deccd5544ac82ed39f5a2aada7ab116493a0619
miguelvent <migueldkj@gmail.com>
2022-04-12 12:31:52 -0300

    bah

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>formulario</title>
+</head>
+<body>
+    <form action="resultado.class.php" method="POST">
+        <label for="lblNome">Nome</label>
+        <input type="text" name="txtnome"><br>
+        <label for="lblemail">Email</label>
+        <input type="text" nome="txtEmail"><br>
+        <button>Cadastrar</button>
+    </form>
+</body>
+</html>
