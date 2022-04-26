3aa20950168088f09fe1b8b4f669462cdd405846
miguelvent <migueldkj@gmail.com>
2022-04-26 12:15:38 -0300

    continuação

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <html>
     <head>
-        <title>formulario</title>
-</head>
-<body>
-    <form action="resultado.class.php" method="POST">
-        <label for="lblNome">Nome</label>
-        <input type="text" name="txtnome"><br>
-        <label for="lblemail">Email</label>
-        <input type="text" nome="txtEmail"><br>
-        <button>Cadastrar</button>
-    </form>
-</body>
+        <title>Formulário</title>
+    </head>
+    <body>
+        <h1>Cadastro Pessoa Física</h1><br>
+        <form method="GET" action="controller.php">
+            <label for="lblNome">Nome:</label><br>
+            <input type="text" name="txtNome"><br>
+            <label for="lblEndereco">Endereço:</label><br>
+            <input type="text" name="txtEndereco"><br>
+            <label for="lblBairro">Bairro:</label><br>
+            <input type="text" name="txtBairro"><br>
+            <label for="lblCep">Cep:</label><br>
+            <input type="text" name="txtCep"><br>
+            <button>Cadastrar</button>
+        </form>
+    </body>
 </html>
